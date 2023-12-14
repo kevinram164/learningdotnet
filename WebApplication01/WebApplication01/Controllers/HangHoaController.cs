84cffadfa75c25d22330ebd1b7a2fdd4bb1d4511
kevinram <trankiet164@gmail.com>
2023-12-14 20:50:31 +0700

    Hello

---
+++ WebApplication01/WebApplication01/Controllers/HangHoaController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication01.Models;
+
+namespace WebApplication01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HangHoaController : Controller
+    {
+        public static List<HangHoa> hangHoas = new List<HangHoa>();
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(hangHoas);
+        }
+
+        [HttpPost]
+        public IActionResult Create(HangHoaVM HangHoaVM)
+        {
+            var hanghoa = new HangHoa
+            {
+                MaHangHoa = Guid.NewGuid(),
+                Name = HangHoaVM.Name,
+                Price = HangHoaVM.Price
+            };
+            hangHoas.Add(hanghoa);
+            return Ok(new
+            {
+                Success = true, Data = hanghoa
+            }
+                );
+        }
+    }
+}
---
+++ WebApplication01/WebApplication01/Models/Products.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication01.Models
+{
+    public class HangHoaVM
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+
+    public class HangHoa : HangHoaVM
+    {
+        public Guid MaHangHoa { get; set; }
+
+    }
+}
